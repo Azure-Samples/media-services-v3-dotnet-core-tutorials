61b13553eb11ee7aad3e7545f74bc66ee5342ded
Julia Kornich <juliako@microsoft.com>
2019-09-18 16:51:48 -0700

    Update Program.cs

--- NETCore/EncodeHTTPAndPublishAESEncrypted/MediaV3ConsoleApp/Program.cs
+++ NETCore/EncodeHTTPAndPublishAESEncrypted/MediaV3ConsoleApp/Program.cs
@@ -446,8 +446,6 @@ namespace EncodeAndStream
             string assetName,
             string outputFolderName)
         {
-            const int ListBlobsSegmentMaxResult = 5;
-
             if (!Directory.Exists(outputFolderName))
             {
                 Directory.CreateDirectory(outputFolderName);
@@ -473,6 +471,11 @@ namespace EncodeAndStream
 
             do
             {
+                // A non-negative integer value that indicates the maximum number of results to be returned at a time,
+                // up to the per-operation limit of 5000. If this value is null, the maximum possible number of results
+                // will be returned, up to 5000.
+                int? ListBlobsSegmentMaxResult = null;    
+                
                 BlobResultSegment segment = await container.ListBlobsSegmentedAsync(null, true, BlobListingDetails.None, ListBlobsSegmentMaxResult, continuationToken, null, null);
 
                 foreach (IListBlobItem blobItem in segment.Results)
